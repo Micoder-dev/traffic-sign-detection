a1a721de0a6409e52b86422363c9706394739373
micoder <micoder.com@gmail.com>
2022-10-30 18:55:48 +0530

    project updated

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ from keras.preprocessing.image import ImageDataGenerator
 path = "myData" # folder with all the class folders
 labelFile = 'labels.csv' # file with all names of classes
 batch_size_val=50  # how many to process together
-steps_per_epoch_val=2000
+steps_per_epoch_val=400
 epochs_val=10
 imageDimesions = (32,32,3)
 testRatio = 0.2    # if 1000 images split will 200 for testing
